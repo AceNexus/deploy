ee3e03bcc3b88f8be29c38387b3f4a50a608a02e
Ace <ooxxqaws@gmail.com>
2026-03-11 16:18:29 +0800

    [chore] 更新部署腳本網路設定並優化 git 忽略清單

--- start_deploy_aiclient.bat
+++ start_deploy_aiclient.bat
@@ -18,6 +18,7 @@ docker run -d ^
   -p 1455:1455 ^
   -p 19876-19880:19876-19880 ^
   --restart=always ^
+  --network acenexus-network ^
   -v "%CONFIGS_DIR%:/app/configs" ^
   --name %CONTAINER_NAME% ^
   %IMAGE_NAME%
